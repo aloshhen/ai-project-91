95b7511c05c2e6205291ea5f615fdc9ca8ad7cf4
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-01-12 00:34:09 +0100

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
+import MenuSection from './components/MenuSection'
+import Gallery from './components/Gallery'
+import Events from './components/Events'
+import BookingForm from './components/BookingForm'
+import Contacts from './components/Contacts'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Hero />
+      <MenuSection />
+      <Gallery />
+      <Events />
+      <BookingForm />
+      <Contacts />
+      <Footer />
+    </>
+  )
+}
+
+export default App
